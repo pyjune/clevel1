49efd2a72bf93bb37b96823a27d1c46803ee7ab9
pyjune <pyjune@naver.com>
2023-01-26 21:52:32 +0900

    1st

---
+++ hello/add/소스.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+//int main(void)
+//{
+//	int a;
+//	int b;
+//	int c;
+//
+//	a = 3;
+//	b = 4;
+//	c = a + b;
+//	printf("%d", c);
+//
+//	return 0;
+//}
+ 
+int main(void)
+{
+	int a = 3;
+	int b = 4;
+	int c;
+
+	c = a + b;
+	printf("a : %d\n", a);
+	printf("b : %d\n", b);
+	printf("c : %d\n", c);
+
+	return 0;
+}
---
+++ hello/hello/소스.cpp
@@ -0,0 +1,29 @@
+/*
+	main() 기본형
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+	printf("hello");
+
+	return 0;
+}
+
+
+/*
+콘솔에서 실행시 실행옵션 전달
+int main(int argc, char* argv[]) {
+	return 0;
+}
+
+OS가 없는 펌웨어의 main함수
+int main(int argc, char* argv[]) {
+
+	while(1)
+	{
+
+	}
+}
+*/
